579a8cdf68629d3a3a2246cb45326612d5b8cf2d
Rohan Modi <rohanmodi1014@gmail.com>
2026-04-13 04:29:30 +0000

    first

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+const int WORD_LENGTH = 5;
+const int NOT_MATCH = 0;
+const int PARTIAL_MATCH = 1;
+const int MATCH = 2;
+
+std::string getRandomWord()
+{
+    // TODO: fetch random word from WordsAPI
+    return "ready";
+}
+
+int main(int, char **)
+{
+int numberOfTries = 6;
+std::vector<std::string> tries(numberOfTries);
+
+}
