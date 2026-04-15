c3703bfb72af64a4a4421f0ce2dd94ce9285261a
Rohan Modi <rohanmodi1014@gmail.com>
2026-04-15 12:12:57 -0500

    Add random word selection functionality
    Implemented random word selection from a predefined list.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <algorithm>
 #include <cctype>
+#include <cstdlib>
+#include <ctime>
 
 const int WORD_LENGTH = 5;
 const int NOT_MATCH = 0;
@@ -11,155 +13,154 @@ const int MATCH = 2;
 
 std::string getRandomWord()
 {
-    // TODO: select random word out of word list
-    /* 
-array
-class
-stack
-queue
-scope
-float
-const
-union
-alias
-block
-ascii
-errno
-ioctl
-scala
-rustc
-swift
-golan
-print
-input
-scanf
-alloc
-frees
-opens
-close
-bytes
-cache
-heapq
-mmapd
-chmod
-chown
-mount
-umask
-httpd
-https
-fetch
-debug
-build
-patch
-merge
-split
-token
-parse
-lexer
-synct
-execs
-forks
-pipes
-event
-await
-async
-yield
-throw
-catch
-funct
-esrch
-while
-final
-types
-macro
-templ
-nodes
-graph
-edges
-index
-value
-param
-argsv
-envrs
-files
-paths
-roots
-loops
-logic
-conds
-flags
-bytes
-words
-bitsy
-clock
-timer
-delay
-input
-write
-reads
-sends
-recvs
-ports
-hosts
-proto
-codec
-frame
-stack
-queue
-deque
-lists
-tuple
-dicts
-sets
-bools
-float
-doubl
-chars
-strng
-regex
-match
-split
-joins
-mapfn
-filtr
-reduz
-sorts
-heaps
-trees
-tries
-graph
-dfsfn
-bfsfn
-visit
-trace
-debug
-logfn
-panic
-crash
-abort
-retry
-sleep
-waker
-sched
-thred
-mutex
-raise
-locks
-rlock
-ulock
-semph
-spawn
-joinr
-tasks
-futur
-proms
-        */
-    return "ready";
+    static const std::vector<std::string> words = {
+        "array",
+        "class",
+        "stack",
+        "queue",
+        "scope",
+        "float",
+        "const",
+        "union",
+        "alias",
+        "block",
+        "ascii",
+        "errno",
+        "ioctl",
+        "scala",
+        "rustc",
+        "swift",
+        "golan",
+        "print",
+        "input",
+        "scanf",
+        "alloc",
+        "frees",
+        "opens",
+        "close",
+        "bytes",
+        "cache",
+        "heapq",
+        "mmapd",
+        "chmod",
+        "chown",
+        "mount",
+        "umask",
+        "httpd",
+        "https",
+        "fetch",
+        "debug",
+        "build",
+        "patch",
+        "merge",
+        "split",
+        "token",
+        "parse",
+        "lexer",
+        "synct",
+        "execs",
+        "forks",
+        "pipes",
+        "event",
+        "await",
+        "async",
+        "yield",
+        "throw",
+        "catch",
+        "funct",
+        "esrch",
+        "while",
+        "final",
+        "types",
+        "macro",
+        "templ",
+        "nodes",
+        "graph",
+        "edges",
+        "index",
+        "value",
+        "param",
+        "files",
+        "paths",
+        "roots",
+        "loops",
+        "logic",
+        "conds",
+        "flags",
+        "words",
+        "clock",
+        "timer",
+        "delay",
+        "write",
+        "reads",
+        "sends",
+        "recvs",
+        "ports",
+        "hosts",
+        "proto",
+        "codec",
+        "frame",
+        "deque",
+        "lists",
+        "tuple",
+        "dicts",
+        "sets",
+        "bools",
+        "doubl",
+        "chars",
+        "strng",
+        "regex",
+        "match",
+        "joins",
+        "mapfn",
+        "filtr",
+        "reduz",
+        "sorts",
+        "heaps",
+        "trees",
+        "tries",
+        "dfsfn",
+        "bfsfn",
+        "visit",
+        "trace",
+        "logfn",                        
+        "panic",
+        "crash",
+        "abort",
+        "retry",
+        "sleep",
+        "waker",
+        "sched",
+        "thred",
+        "mutex",
+        "raise",
+        "locks",
+        "rlock",
+        "ulock",
+        "semph",
+        "spawn",
+        "joinr",
+        "tasks",
+        "futur",
+        "proms"
+    };
+  static bool seeded = false;
+    if (!seeded) {
+        std::srand(static_cast<unsigned>(std::time(nullptr)));
+        seeded = true;
+    }
+
+    return words[std::rand() % words.size()];
 }
 
 int main(int, char **)
 {
-int numberOfTries = 6;
-std::vector<std::string> tries(numberOfTries);
+    int numberOfTries = 6;
+    std::vector<std::string> tries(numberOfTries);
 
+    std::string target = getRandomWord();
+
+    std::cout << target << std::endl;
+
+    return 0;
 }
